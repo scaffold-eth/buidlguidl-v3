6d0e1ebdb2147c343a466883d61055cec1925169
Carlos Sánchez <oceanrdn@gmail.com>
2022-02-11 09:50:09 +0100

    Fix #6 respect line break on build desc

--- packages/react-app/src/components/BuildCard.jsx
+++ packages/react-app/src/components/BuildCard.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import { Image, Link, Box, Flex, Button, Center, ButtonGroup, Text, Spacer } from "@chakra-ui/react";
+import { Image, Link, Box, Flex, Button, Center, Text, Spacer } from "@chakra-ui/react";
 import useCustomColorModes from "../hooks/useCustomColorModes";
 
 const BuildCard = ({ build }) => {
   const { borderColor, secondaryFontColor } = useCustomColorModes();
   return (
-    <Box borderWidth="1px" borderRadius="lg" borderColor={borderColor} overflow="hidden">
+    <Box
+      borderWidth="1px"
+      borderRadius="lg"
+      borderColor={borderColor}
+      overflow="hidden"
+      display="flex"
+      flexDirection="column"
+    >
       <Box bgColor={borderColor} borderBottom="1px" borderColor={borderColor}>
         {build.image ? <Image src={build.image} h="200px" mx="auto" /> : <Center h="200px">No image</Center>}
       </Box>
-      <Flex pt={9} pb={4} px={4} direction="column" minH="240px">
+      <Flex pt={9} pb={4} px={4} direction="column" minH="240px" h="100%">
         <Text fontWeight="bold">{build.name}</Text>
-        <Text color={secondaryFontColor}>{build.desc}</Text>
+        <Text color={secondaryFontColor} whiteSpace="pre-wrap">
+          {build.desc}
+        </Text>
         <Spacer />
-        <ButtonGroup variant="outline" size="sm" spacing="2">
-          <Button isFullWidth as={Link} isExternal href={build.branch} _hover={{ textDecoration: "none" }}>
-            Fork
-          </Button>
-        </ButtonGroup>
+        <Button mt={3} variant="outline" size="sm" as={Link} isExternal href={build.branch} isFullWidth>
+          Fork
+        </Button>
       </Flex>
     </Box>
   );
