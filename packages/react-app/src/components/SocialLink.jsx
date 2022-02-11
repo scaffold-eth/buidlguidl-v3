bd8ac3115f0c7edb165cf0f5bf02d5b6e5d787dd
Carlos Sánchez <oceanrdn@gmail.com>
2022-02-11 09:28:43 +0100

    Fix #8 flip social svg colors on dark mode

--- packages/react-app/src/components/SocialLink.jsx
+++ packages/react-app/src/components/SocialLink.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { Box, Link, Tooltip, useClipboard } from "@chakra-ui/react";
+import { Box, Link, Tooltip, useClipboard, useColorModeValue } from "@chakra-ui/react";
 import { socials } from "../data/socials";
 
 const SocialLink = ({ id, value }) => {
   const Icon = socials[id].icon;
   const { hasCopied, onCopy } = useClipboard(value);
   const link = socials[id].getLink(value);
+  const svgFill = useColorModeValue("black", "white");
 
   return (
     <>
       {link ? (
         <Link href={socials[id].getLink(value)} isExternal>
-          <Icon w={4} />
+          <Icon w={4} fill={svgFill} />
         </Link>
       ) : (
         <Tooltip label="Copied to your clipboard!" isOpen={hasCopied}>
           <Box cursor="pointer" onClick={onCopy}>
-            <Icon w={4} label={value} />
+            <Icon w={4} label={value} fill={svgFill} />
           </Box>
         </Tooltip>
       )}
