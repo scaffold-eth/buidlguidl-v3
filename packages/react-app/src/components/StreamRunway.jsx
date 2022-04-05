3c738c37c6f4c37bb93b537f4db9a449f64fcb97
Carlos Sánchez <oceanrdn@gmail.com>
2022-04-05 18:30:57 +0200

    Show stream balance in red when unlocked > balance

--- packages/react-app/src/components/StreamRunway.jsx
+++ packages/react-app/src/components/StreamRunway.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Box, Flex } from "@chakra-ui/react";
+import { ethers } from "ethers";
 
 const StreamRunway = ({ stream }) => {
   if (!stream || !stream?.cap) return <Box>-</Box>;
 
-  const cap = parseFloat(stream.cap);
-  const balance = parseFloat(stream.balance);
-  const isBalanceUnderCap = balance < cap;
+  const cap = ethers.utils.parseUnits(stream.cap);
+  const frequency = stream.frequency;
+  const last = stream.lastContract;
+  const unlockedAmount = cap.mul(Math.round(new Date().getTime() / 1000 - last)).div(frequency);
+  const available = cap.lt(unlockedAmount) ? cap : unlockedAmount;
+
+  const isBalanceUnderAvailable = parseFloat(stream.balance) < ethers.utils.formatEther(available);
 
   return (
     <Flex align="center" justify="center" direction="column" px={4} mt={4}>
-      <Box mb={1} color={isBalanceUnderCap ? "red.500" : ""}>
+      <Box mb={1} color={isBalanceUnderAvailable ? "red.500" : ""}>
         Balance: Ξ {parseFloat(stream.balance).toFixed(4)}
       </Box>
     </Flex>
