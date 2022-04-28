09c827d66207642f04f26fe78ea6049c9428e63a
Carlos Sánchez <oceanrdn@gmail.com>
2022-04-28 12:41:05 +0200

    Build search

--- packages/react-app/src/views/BuildsView.jsx
+++ packages/react-app/src/views/BuildsView.jsx
@@ -1,5 +1,18 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Button, Container, SimpleGrid, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Input,
+  InputGroup,
+  InputRightElement,
+  SimpleGrid,
+  Spinner,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 import { getAllBuilds } from "../data/api";
 import BuildCard from "../components/BuildCard";
 import SubmitBuildModal from "../components/SubmitBuildModal";
@@ -9,6 +22,8 @@ const MIN_LIKES = 1;
 
 export default function BuildsView({ userProvider, connectedBuilder, userRole }) {
   const [builds, setBuilds] = useState([]);
+  const [isLoadingBuilds, setIsLoadingBuilds] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const updateBuilds = useCallback(async () => {
@@ -16,37 +31,69 @@ export default function BuildsView({ userProvider, connectedBuilder, userRole })
     // Now we only show builds with like
     // Fetching all for now (and filtering them in the front)
     // but we could implement getAllLikedBuilds(minNumber)
+    setIsLoadingBuilds(true);
     const allBuilds = await getAllBuilds();
 
     const sortedBuilds = allBuilds
       .filter(build => !build.isDraft && build.likes?.length >= MIN_LIKES)
       .sort((a, b) => b.likes.length - a.likes.length);
+
     setBuilds(sortedBuilds);
+    setIsLoadingBuilds(false);
   }, []);
 
   useEffect(() => {
     updateBuilds();
   }, [updateBuilds]);
 
+  useEffect(() => {}, [searchQuery]);
+
+  const searchFilter = build => {
+    if (searchQuery.length < 3) return true;
+
+    const buildTitle = build.name?.toLowerCase();
+    const buildDescription = build.desc?.toLowerCase();
+    const lowerCaseSearch = searchQuery.toLowerCase();
+
+    return buildTitle.includes(lowerCaseSearch) || buildDescription.includes(lowerCaseSearch);
+  };
+
+  const buildsToRender = builds.filter(searchFilter);
+
   return (
     <Container maxW="container.lg" centerContent>
       {connectedBuilder && (
-        <Button colorScheme="blue" mb={8} onClick={onOpen}>
+        <Button colorScheme="blue" mb={6} onClick={onOpen}>
           Submit New Build
         </Button>
       )}
 
-      <SimpleGrid columns={[1, null, 2, null, 3]} spacing={6} pb={20}>
-        {builds.map(build => (
-          <BuildCard
-            build={build}
-            key={build.id}
-            userProvider={userProvider}
-            onUpdate={updateBuilds}
-            userRole={userRole}
-          />
-        ))}
-      </SimpleGrid>
+      <Box mb={8}>
+        <InputGroup>
+          <Input placeholder="Search builds" onChange={event => setSearchQuery(event.target.value)} />
+          <InputRightElement pointerEvents="none" color="gray.300" fontSize="1.2em" children={<SearchIcon />} />
+        </InputGroup>
+      </Box>
+
+      {isLoadingBuilds ? (
+        <Spinner />
+      ) : buildsToRender.length ? (
+        <SimpleGrid columns={[1, null, 2, null, 3]} spacing={6} pb={20}>
+          {buildsToRender.map(build => (
+            <BuildCard
+              build={build}
+              key={build.id}
+              userProvider={userProvider}
+              onUpdate={updateBuilds}
+              userRole={userRole}
+            />
+          ))}
+        </SimpleGrid>
+      ) : (
+        <Center mt={6}>
+          <Text color="gray.500">- No builds found -</Text>
+        </Center>
+      )}
 
       <SubmitBuildModal isOpen={isOpen} onClose={onClose} />
     </Container>
