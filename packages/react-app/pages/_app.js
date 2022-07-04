5e6459d1d9b169d600d6bcc353ebf5966b37b59e
Carlos Sánchez <oceanrdn@gmail.com>
2022-07-04 18:25:18 +0200

    NextJS move common functionality to _app.js

--- packages/react-app/pages/_app.js
+++ packages/react-app/pages/_app.js
@@ -1,15 +1,228 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "../index.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { InfuraProvider, StaticJsonRpcProvider, Web3Provider } from "@ethersproject/providers";
+import { useUserAddress } from "eth-hooks";
+import axios from "axios";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 import theme from "../theme";
+import BlockchainProvidersContext from "../contexts/blockchainProvidersContext";
+import { ColorModeSwitcher, Header } from "../components";
+import TelegramJoin from "../components/TelegramJoin";
+import { providerPromiseWrapper } from "../helpers/blockchainProviders";
+import { INFURA_ID, SERVER_URL as serverUrl } from "../constants";
+import { useUserProvider } from "../hooks";
+import { USER_ROLES } from "../helpers/constants";
+
+const DEBUG = true;
+
+/*
+  Web3 modal helps us "connect" external wallets:
+*/
+let web3Modal;
+if (typeof window !== "undefined") {
+  web3Modal = new Web3Modal({
+    // network: "mainnet", // optional
+    cacheProvider: true, // optional
+    providerOptions: {
+      walletconnect: {
+        package: WalletConnectProvider, // required
+        options: {
+          infuraId: INFURA_ID,
+        },
+      },
+    },
+  });
+}
+
+const providerPromiseResolvers = {
+  mainnet: {
+    resolve: () => {},
+    reject: () => {},
+  },
+  user: {
+    resolve: () => {},
+    reject: () => {},
+  },
+};
 
 function MyApp({ Component, pageProps }) {
+  const [providers, setProviders] = useState({
+    mainnet: {
+      provider: null,
+      isReady: false,
+      providerPromise: new Promise((resolve, reject) => {
+        providerPromiseResolvers.mainnet = { resolve, reject };
+      }),
+    },
+    local: { provider: null, isReady: false },
+    user: {
+      provider: null,
+      isReady: false,
+      providerPromise: new Promise((resolve, reject) => {
+        providerPromiseResolvers.user = { resolve, reject };
+      }),
+    },
+  });
+
+  useEffect(() => {
+    // 🛰 providers
+
+    if (DEBUG) console.log("📡 Connecting to Mainnet Ethereum");
+    const scaffoldEthProviderPromise = providerPromiseWrapper(
+      new StaticJsonRpcProvider("https://rpc.scaffoldeth.io:48544"),
+    );
+
+    // attempt to connect to our own scaffold eth rpc and if that fails fall back to infura...
+    // Using StaticJsonRpcProvider as the chainId won't change see https://github.com/ethers-io/ethers.js/issues/901
+    scaffoldEthProviderPromise
+      .then(provider => {
+        if (DEBUG) console.log("📡 Connected to Mainnet Ethereum using the scaffold eth provider");
+        providerPromiseResolvers.mainnet.resolve(provider);
+        setProviders(prevProviders => ({ ...prevProviders, mainnet: { provider, isReady: true } }));
+      })
+      .catch(() => {
+        if (DEBUG) console.log("❌ 📡 Connection to Mainnet Ethereum using the scaffold eth provider failed");
+        const mainnetInfuraProviderPromise = providerPromiseWrapper(new InfuraProvider("mainnet", INFURA_ID));
+        mainnetInfuraProviderPromise
+          .then(provider => {
+            if (DEBUG) console.log("📡 Connected to Mainnet Ethereum using the infura provider as callback");
+            providerPromiseResolvers.mainnet.resolve(provider);
+            setProviders(prevProviders => ({ ...prevProviders, mainnet: { provider, isReady: true } }));
+          })
+          .catch(() => {
+            if (DEBUG) console.log("❌ 📡 Connection to Mainnet Ethereum using the infura provider as fallback failed");
+            // ( ⚠️ Getting "failed to meet quorum" errors? Check your INFURA_ID)
+            providerPromiseResolvers.mainnet.reject(
+              "❌ 📡 Connection to Mainnet Ethereum using the infura provider as fallback failed",
+            );
+          });
+      });
+  }, []);
+
+  const mainnetProvider = providers.mainnet?.provider;
+
+  const [injectedProvider, setInjectedProvider] = useState();
+
+  // Use your injected provider from 🦊 Metamask or if you don't have it then instantly generate a 🔥 burner wallet.
+  const userProvider = useUserProvider(injectedProvider);
+
+  useEffect(() => {
+    if (!userProvider) {
+      return;
+    }
+    providerPromiseResolvers.user.resolve(userProvider);
+    setProviders(prevProviders => ({ ...prevProviders, user: { provider: userProvider, isReady: true } }));
+  }, [setProviders, userProvider]);
+
+  // TODO address is derived from userProvider, so we should just send userProvider
+  const address = useUserAddress(userProvider);
+
+  // You can warn the user if you would like them to be on a specific network
+  const selectedChainId = userProvider && userProvider._network && userProvider._network.chainId;
+
+  //
+  // 🧫 DEBUG 👨🏻‍🔬
+  //
+  useEffect(() => {
+    if (DEBUG && mainnetProvider && address && selectedChainId) {
+      console.log("_____________________________________ 🏗 scaffold-eth _____________________________________");
+      console.log("🌎 mainnetProvider", mainnetProvider);
+      console.log("👩‍💼 selected address:", address);
+      console.log("🕵🏻‍♂️ selectedChainId:", selectedChainId);
+    }
+  }, [mainnetProvider, address, selectedChainId]);
+
+  const loadWeb3Modal = useCallback(async () => {
+    const provider = await web3Modal.connect();
+    setInjectedProvider(new Web3Provider(provider));
+  }, [setInjectedProvider]);
+
+  const logoutOfWeb3Modal = async () => {
+    await web3Modal.clearCachedProvider();
+    if (injectedProvider && injectedProvider.provider && typeof injectedProvider.provider.disconnect == "function") {
+      await injectedProvider.provider.disconnect();
+    }
+    setTimeout(() => {
+      if (typeof window !== "undefined") {
+        window.location.reload();
+      }
+    }, 1);
+  };
+
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      loadWeb3Modal();
+    }
+  }, [loadWeb3Modal]);
+
+  // ToDo. We could merge these two states.
+  const [userRole, setUserRole] = useState(null);
+  const [connectedBuilder, setConnectedBuilder] = useState(null);
+
+  const fetchUserData = useCallback(async () => {
+    console.log("getting user data");
+    try {
+      const fetchedUserObject = await axios.get(serverUrl + `/builders/${address}`);
+      setUserRole(USER_ROLES[fetchedUserObject.data.role] ?? USER_ROLES.anonymous);
+      setConnectedBuilder(fetchedUserObject.data);
+    } catch (e) {
+      setUserRole(USER_ROLES.anonymous);
+    }
+  }, [address]);
+
+  useEffect(() => {
+    if (address) {
+      fetchUserData();
+    }
+  }, [address, fetchUserData]);
+
   return (
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Component {...pageProps} />
+      <BlockchainProvidersContext.Provider value={providers}>
+        <div className="App">
+          {/* ✏️ Edit the header and change the title to your project name */}
+          <Header
+            injectedProvider={injectedProvider}
+            userRole={userRole}
+            address={address}
+            mainnetProvider={mainnetProvider}
+            userProvider={userProvider}
+            loadWeb3Modal={loadWeb3Modal}
+            logoutOfWeb3Modal={logoutOfWeb3Modal}
+            setUserRole={setUserRole}
+          />
+          <TelegramJoin connectedBuilder={connectedBuilder} />
+          <Component {...pageProps} />
+          <ColorModeSwitcher />
+        </div>
+      </BlockchainProvidersContext.Provider>
     </ChakraProvider>
   );
+}
+
+if (typeof window !== "undefined" && window.ethereum) {
+  window.ethereum.on("chainChanged", () => {
+    if (web3Modal.cachedProvider) {
+      setTimeout(() => {
+        if (typeof window !== "undefined") {
+          window.location.reload();
+        }
+      }, 1);
+    }
+  });
+
+  window.ethereum.on("accountsChanged", () => {
+    if (web3Modal.cachedProvider) {
+      setTimeout(() => {
+        if (typeof window !== "undefined") {
+          window.location.reload();
+        }
+      }, 1);
+    }
+  });
 }
 
 export default MyApp;
--- packages/react-app/pages/index.jsx
+++ packages/react-app/pages/index.jsx
@@ -1,304 +1,243 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Web3Provider, StaticJsonRpcProvider, InfuraProvider } from "@ethersproject/providers";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import { Center, Text } from "@chakra-ui/react";
-import { useUserAddress } from "eth-hooks";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useUserProvider } from "../hooks";
-import { Header, ColorModeSwitcher } from "../components";
-import { INFURA_ID, SERVER_URL as serverUrl } from "../constants";
 import {
-  BuilderListView,
-  BuilderProfileView,
-  BuilderCreateView,
-  AllBuildsReviewView,
-  BuildsView,
-  ActivityView,
-  WithdrawStats,
-  EnsClaimsView,
-  HomepageView,
-} from "../views";
-import { USER_ROLES } from "../helpers/constants";
-import { providerPromiseWrapper } from "../helpers/blockchainProviders";
-import BlockchainProvidersContext from "../contexts/blockchainProvidersContext";
-import BuildDetailView from "../views/BuildDetailView";
-import BuildVoteList from "../views/BuildVoteList";
-import TelegramJoin from "../components/TelegramJoin";
+  Box,
+  HStack,
+  Heading,
+  Text,
+  Link,
+  Image,
+  chakra,
+  Container,
+  Button,
+  Spinner,
+  Flex,
+  useColorModeValue,
+  useColorMode,
+} from "@chakra-ui/react";
+import BuilderFunctionList from "../components/BuilderFunctionList";
+import { SERVER_URL } from "../constants";
+import { USER_FUNCTIONS } from "../helpers/constants";
 
-// import dynamic from "next/dynamic";
-// const Web3Modal = dynamic(() => import("web3modal"), { ssr: false });
-import Web3Modal from "web3modal";
+const buildersToShow = ["fullstack", "frontend", "damageDealer", "advisor", "artist", "support"];
 
-// 😬 Sorry for all the console logging
-const DEBUG = true;
+/* eslint-disable jsx-a11y/accessible-emoji */
+export default function Index() {
+  const [builders, setBuilders] = useState([]);
+  const [isLoadingBuilders, setIsLoadingBuilders] = useState(false);
 
-/*
-  Web3 modal helps us "connect" external wallets:
-*/
-let web3Modal;
-if (typeof window !== "undefined") {
-  web3Modal = new Web3Modal({
-    // network: "mainnet", // optional
-    cacheProvider: true, // optional
-    providerOptions: {
-      walletconnect: {
-        package: WalletConnectProvider, // required
-        options: {
-          infuraId: INFURA_ID,
-        },
-      },
-    },
-  });
-}
-
-const providerPromiseResolvers = {
-  mainnet: {
-    resolve: () => {},
-    reject: () => {},
-  },
-  user: {
-    resolve: () => {},
-    reject: () => {},
-  },
-};
-
-function Index() {
-  const [providers, setProviders] = useState({
-    mainnet: {
-      provider: null,
-      isReady: false,
-      providerPromise: new Promise((resolve, reject) => {
-        providerPromiseResolvers.mainnet = { resolve, reject };
-      }),
-    },
-    local: { provider: null, isReady: false },
-    user: {
-      provider: null,
-      isReady: false,
-      providerPromise: new Promise((resolve, reject) => {
-        providerPromiseResolvers.user = { resolve, reject };
-      }),
-    },
-  });
+  const { colorMode } = useColorMode();
+  const isDarkMode = colorMode === "dark";
+  const heroBg = useColorModeValue("#FCFBF8", "gray.800");
+  const scaffoldEthBg = useColorModeValue("#fbf7f6", "whiteAlpha.300");
 
   useEffect(() => {
-    // 🛰 providers
+    async function fetchBuilders() {
+      setIsLoadingBuilders(true);
+      const fetchedBuilders = await axios.get(`${SERVER_URL}/builders`);
 
-    /*
-    //This code is needed if you want a local and a mainnet provider
+      setBuilders(fetchedBuilders.data);
+      setIsLoadingBuilders(false);
+    }
 
-    // 📡 What chain are your contracts deployed to?
-    const targetNetwork = NETWORKS.mainnet; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
-
-    // 🏠 Your local provider is usually pointed at your local blockchain
-    const localProviderUrl = targetNetwork.rpcUrl;
-
-    // as you deploy to other networks you can set REACT_APP_PROVIDER=https://dai.poa.network in packages/react-app/.env
-    const localProviderUrlFromEnv = process.env.REACT_APP_PROVIDER ? process.env.REACT_APP_PROVIDER : localProviderUrl;
-
-    const localProviderPromise = providerPromiseWrapper(new StaticJsonRpcProvider(localProviderUrlFromEnv));
-    */
-
-    if (DEBUG) console.log("📡 Connecting to Mainnet Ethereum");
-    const scaffoldEthProviderPromise = providerPromiseWrapper(
-      new StaticJsonRpcProvider("https://rpc.scaffoldeth.io:48544"),
-    );
-
-    // attempt to connect to our own scaffold eth rpc and if that fails fall back to infura...
-    // Using StaticJsonRpcProvider as the chainId won't change see https://github.com/ethers-io/ethers.js/issues/901
-    scaffoldEthProviderPromise
-      .then(provider => {
-        if (DEBUG) console.log("📡 Connected to Mainnet Ethereum using the scaffold eth provider");
-        providerPromiseResolvers.mainnet.resolve(provider);
-        setProviders(prevProviders => ({ ...prevProviders, mainnet: { provider, isReady: true } }));
-      })
-      .catch(() => {
-        if (DEBUG) console.log("❌ 📡 Connection to Mainnet Ethereum using the scaffold eth provider failed");
-        const mainnetInfuraProviderPromise = providerPromiseWrapper(new InfuraProvider("mainnet", INFURA_ID));
-        mainnetInfuraProviderPromise
-          .then(provider => {
-            if (DEBUG) console.log("📡 Connected to Mainnet Ethereum using the infura provider as callback");
-            providerPromiseResolvers.mainnet.resolve(provider);
-            setProviders(prevProviders => ({ ...prevProviders, mainnet: { provider, isReady: true } }));
-          })
-          .catch(() => {
-            if (DEBUG) console.log("❌ 📡 Connection to Mainnet Ethereum using the infura provider as fallback failed");
-            // ( ⚠️ Getting "failed to meet quorum" errors? Check your INFURA_ID)
-            providerPromiseResolvers.mainnet.reject(
-              "❌ 📡 Connection to Mainnet Ethereum using the infura provider as fallback failed",
-            );
-          });
-      });
+    fetchBuilders();
   }, []);
 
-  const mainnetProvider = providers.mainnet?.provider;
-
-  const [injectedProvider, setInjectedProvider] = useState();
-
-  // Use your injected provider from 🦊 Metamask or if you don't have it then instantly generate a 🔥 burner wallet.
-  const userProvider = useUserProvider(injectedProvider);
-
-  useEffect(() => {
-    if (!userProvider) {
-      return;
-    }
-    providerPromiseResolvers.user.resolve(userProvider);
-    setProviders(prevProviders => ({ ...prevProviders, user: { provider: userProvider, isReady: true } }));
-  }, [setProviders, userProvider]);
-
-  // TODO address is derived from userProvider, so we should just send userProvider
-  const address = useUserAddress(userProvider);
-
-  // You can warn the user if you would like them to be on a specific network
-  const selectedChainId = userProvider && userProvider._network && userProvider._network.chainId;
-
-  //
-  // 🧫 DEBUG 👨🏻‍🔬
-  //
-  useEffect(() => {
-    if (DEBUG && mainnetProvider && address && selectedChainId) {
-      console.log("_____________________________________ 🏗 scaffold-eth _____________________________________");
-      console.log("🌎 mainnetProvider", mainnetProvider);
-      console.log("👩‍💼 selected address:", address);
-      console.log("🕵🏻‍♂️ selectedChainId:", selectedChainId);
-    }
-  }, [mainnetProvider, address, selectedChainId]);
-
-  const loadWeb3Modal = useCallback(async () => {
-    const provider = await web3Modal.connect();
-    setInjectedProvider(new Web3Provider(provider));
-  }, [setInjectedProvider]);
-
-  const logoutOfWeb3Modal = async () => {
-    await web3Modal.clearCachedProvider();
-    if (injectedProvider && injectedProvider.provider && typeof injectedProvider.provider.disconnect == "function") {
-      await injectedProvider.provider.disconnect();
-    }
-    setTimeout(() => {
-      if (typeof window !== "undefined") {
-        window.location.reload();
-      }
-    }, 1);
-  };
-
-  useEffect(() => {
-    if (web3Modal.cachedProvider) {
-      loadWeb3Modal();
-    }
-  }, [loadWeb3Modal]);
-
-  // ToDo. We could merge these two states.
-  const [userRole, setUserRole] = useState(null);
-  const [connectedBuilder, setConnectedBuilder] = useState(null);
-
-  const fetchUserData = useCallback(async () => {
-    console.log("getting user data");
-    try {
-      const fetchedUserObject = await axios.get(serverUrl + `/builders/${address}`);
-      setUserRole(USER_ROLES[fetchedUserObject.data.role] ?? USER_ROLES.anonymous);
-      setConnectedBuilder(fetchedUserObject.data);
-    } catch (e) {
-      setUserRole(USER_ROLES.anonymous);
-    }
-  }, [address]);
-
-  useEffect(() => {
-    if (address) {
-      fetchUserData();
-    }
-  }, [address, fetchUserData]);
-
   return (
-    <BlockchainProvidersContext.Provider value={providers}>
-      <div className="App">
-        {/* ✏️ Edit the header and change the title to your project name */}
-        <Header
-          injectedProvider={injectedProvider}
-          userRole={userRole}
-          address={address}
-          mainnetProvider={mainnetProvider}
-          userProvider={userProvider}
-          loadWeb3Modal={loadWeb3Modal}
-          logoutOfWeb3Modal={logoutOfWeb3Modal}
-          setUserRole={setUserRole}
-        />
-        <TelegramJoin connectedBuilder={connectedBuilder} />
-        <HomepageView />
-        {/*<Switch>*/}
-        {/*  <Route exact path="/">*/}
-        {/*    <HomepageView />*/}
-        {/*  </Route>*/}
-        {/*  <Route exact path="/activity">*/}
-        {/*    <ActivityView />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/builds" exact>*/}
-        {/*    <BuildsView userProvider={userProvider} connectedBuilder={connectedBuilder} userRole={userRole} />*/}
-        {/*  </Route>*/}
-        {/*  <Route exact path="/portfolio">*/}
-        {/*    {address && <Redirect to={"/builders/" + address} />}*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/builders" exact>*/}
-        {/*    <BuilderListView serverUrl={serverUrl} mainnetProvider={mainnetProvider} userRole={userRole} />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/builders/:builderAddress">*/}
-        {/*    <BuilderProfileView*/}
-        {/*      serverUrl={serverUrl}*/}
-        {/*      mainnetProvider={mainnetProvider}*/}
-        {/*      address={address}*/}
-        {/*      userRole={userRole}*/}
-        {/*      userProvider={userProvider}*/}
-        {/*    />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/build/:buildId" exact>*/}
-        {/*    <BuildDetailView />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/builds/vote" exact>*/}
-        {/*    <BuildVoteList />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/admin/add-builder" exact>*/}
-        {/*    <BuilderCreateView mainnetProvider={mainnetProvider} />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/admin/ens-claims" exact>*/}
-        {/*    <EnsClaimsView />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/admin/withdraw-stats" exact>*/}
-        {/*    <WithdrawStats />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/admin/builds-review" exact>*/}
-        {/*    <AllBuildsReviewView userProvider={userProvider} mainnetProvider={mainnetProvider} />*/}
-        {/*  </Route>*/}
-        {/*  <Route exact path="/404">*/}
-        {/*    <Center>*/}
-        {/*      <Text fontSize="xl">Not found :(</Text>*/}
-        {/*    </Center>*/}
-        {/*  </Route>*/}
-        {/*</Switch>*/}
-        <ColorModeSwitcher />
-      </div>
-    </BlockchainProvidersContext.Provider>
+    <>
+      <Flex bgColor={heroBg} alignItems="center" direction={{ base: "column-reverse", lg: "row" }}>
+        <Box w={{ base: "100%", lg: "50%" }} pl={{ base: "50px", lg: "15%" }} pr="50px" py="50px">
+          <Box maxW="470px" margin={{ base: "auto", lg: "0" }} textAlign={{ base: "center", lg: "left" }}>
+            <Heading as="h1" mb="10px">
+              BuidlGuidl <chakra.span color="#CBD5E0">v3</chakra.span>
+            </Heading>
+            <Text mb="25px">
+              A curated group of <strong>Ethereum</strong> builders creating products, prototypes, and tutorials to
+              enrich the web3 ecosystem.
+            </Text>
+            <Text mb="10px">❤️ We are an Ethereum public good.</Text>
+          </Box>
+        </Box>
+
+        <Box w={{ base: "100%", lg: "50%" }} mt={{ base: "50px", lg: "0" }}>
+          <Image
+            src="assets/bg_castle.png"
+            p={{ base: "0", lg: "80px" }}
+            m="auto"
+            maxW={{ base: "200px", lg: "100%" }}
+          />
+        </Box>
+      </Flex>
+
+      <Box bgColor={scaffoldEthBg} p="45px 0">
+        <Container maxW="container.md" centerContent>
+          <Flex alignItems="center" flexDirection={{ base: "column", lg: "row" }}>
+            <Image
+              src="assets/scaffold-eth.jpeg"
+              pb={{ base: "25px", lg: "0" }}
+              m="auto"
+              maxW="200px"
+              filter={isDarkMode ? "grayscale(1)" : "auto"}
+            />
+            <Box pl={{ base: "0", lg: "80px" }}>
+              <Text fontSize={{ base: "xl", lg: "2xl" }} fontWeight="bold">
+                <Link href="https://github.com/scaffold-eth/scaffold-eth" fontWeight="700" color="teal.500" isExternal>
+                  🏗 scaffold-eth
+                </Link>{" "}
+                is our tool of choice.
+              </Text>
+              <Text fontSize={{ base: "md", lg: "lg" }} mt="8px">
+                🚀 Everything you need to build on Ethereum!
+              </Text>
+              <Text fontSize={{ base: "md", lg: "lg" }} mt="8px">
+                🔧 We actively maintain it and build with it.
+              </Text>
+            </Box>
+          </Flex>
+        </Container>
+      </Box>
+
+      <Container maxW="container.md" centerContent>
+        <Box textAlign="center" mt="128px">
+          <Heading as="h3" color="#CBD5E0" mb="10px">
+            0.
+          </Heading>
+          <Text>
+            👋 Are you a <b>developer</b> onboarding into web3?
+          </Text>
+          <Text mt="18px">
+            ⚔️ Take on the challenges over at{" "}
+            <Link href="https://speedrunethereum.com" color="teal.500" isExternal>
+              SpeedRunEthereum.com
+            </Link>
+            !
+          </Text>
+        </Box>
+
+        <Box textAlign="center" mt="128px">
+          <Heading as="h3" color="#CBD5E0" mb="10px">
+            1.
+          </Heading>
+          <Text>
+            🏭 So you can crush Solidity and you're looking for{" "}
+            <Link
+              href="https://twitter.com/austingriffith/status/1478760479275175940?s=20&t=0zGF8M_7Hoeuy-D6LDoFpA"
+              color="teal.500"
+              isExternal
+            >
+              next steps
+            </Link>
+            ?
+          </Text>
+          <Text mt="18px">
+            🧫 Filter by{" "}
+            <Link
+              href="https://github.com/scaffold-eth/scaffold-eth-examples/branches/active"
+              color="teal.500"
+              isExternal
+            >
+              active
+            </Link>{" "}
+            in the{" "}
+            <Link href="https://github.com/scaffold-eth/scaffold-eth-examples" color="teal.500" isExternal>
+              scaffold-eth-examples
+            </Link>{" "}
+            branch...
+          </Text>
+          <Text mt="18px">
+            🧑‍🚀 contribute to an interesting build or{" "}
+            <Link href="https://github.com/scaffold-eth/scaffold-eth#-scaffold-eth" color="teal.500" isExternal>
+              fork
+            </Link>{" "}
+            something new!
+          </Text>
+        </Box>
+
+        <Box textAlign="center" mt="128px">
+          <Heading as="h3" color="#CBD5E0" mb="10px">
+            2.
+          </Heading>
+          <Text>
+            🏹 Are you <b>building</b> forkable components with 🏗 scaffold-eth?
+          </Text>
+          <Text mt="18px">
+            ⚖️ Shill your wares at the{" "}
+            <Link color="teal.500" href="/builds">
+              🏰 BuidlGuidl
+            </Link>
+          </Text>
+        </Box>
+
+        <Box textAlign="center" mt="128px">
+          <Heading as="h3" color="#CBD5E0" mb="10px">
+            3.
+          </Heading>
+          <Text>
+            💬 Chat with fellow builders in the{" "}
+            <Link color="teal.500" isExternal href="https://t.me/+PXu_P6pps5I5ZmUx">
+              🏰 BuidlGuidl 📣 Townhall
+            </Link>{" "}
+            telegram!
+          </Text>
+        </Box>
+
+        <Box textAlign="center" mt="64px">
+          <Text>-</Text>
+        </Box>
+
+        <Box textAlign="center" mt="64px">
+          <Text mb="25px">
+            💰 a{" "}
+            <Link href="https://fund.buidlguidl.com/funding" fontWeight="700" color="teal.500" isExternal>
+              yolo
+            </Link>{" "}
+            to 🏰BuidlGuidl.eth is a{" "}
+            <Link href="https://fund.buidlguidl.com/funding" fontWeight="700" color="teal.500" isExternal>
+              yolo
+            </Link>{" "}
+            to high leverage web3 devs.
+          </Text>
+        </Box>
+
+        <Box textAlign="center" mt="64px">
+          <Text>-</Text>
+        </Box>
+
+        <Heading as="h2" size="md" my="64px" color="gray.500">
+          Active 🏰 BuidlGuidl members:
+        </Heading>
+
+        {buildersToShow.map(builderFunction => (
+          <Box mb="144px">
+            <HStack justifyContent="center" mb="25px" spacing="24px">
+              <Image src={`/assets/${USER_FUNCTIONS[builderFunction]?.graphic}`} boxSize="200px" />
+              <Heading as="h3" color="gray.500" w="160px">
+                {USER_FUNCTIONS[builderFunction]?.pluralLabel}
+              </Heading>
+            </HStack>
+            {isLoadingBuilders ? (
+              <Flex justifyContent="center">
+                <Spinner />
+              </Flex>
+            ) : (
+              <BuilderFunctionList builders={builders.filter(builder => builder.function === builderFunction)} />
+            )}
+          </Box>
+        ))}
+
+        <Box>
+          <Button as="a" href="/builders" colorScheme="blue">
+            View all Builders
+          </Button>
+        </Box>
+
+        <Box mt="128px" mb="25px">
+          🏰<b>BuidlGuidl</b> is a registered 🤠{" "}
+          <Link href="https://dao.buidlguidl.com/" fontWeight="700" color="teal.500" isExternal>
+            Wyoming DAO LLC
+          </Link>
+        </Box>
+      </Container>
+    </>
   );
 }
-
-if (typeof window !== "undefined" && window.ethereum) {
-  window.ethereum.on("chainChanged", () => {
-    if (web3Modal.cachedProvider) {
-      setTimeout(() => {
-        if (typeof window !== "undefined") {
-          window.location.reload();
-        }
-      }, 1);
-    }
-  });
-
-  window.ethereum.on("accountsChanged", () => {
-    if (web3Modal.cachedProvider) {
-      setTimeout(() => {
-        if (typeof window !== "undefined") {
-          window.location.reload();
-        }
-      }, 1);
-    }
-  });
-}
-
-export default Index;
