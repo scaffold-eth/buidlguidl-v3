cda02120d74fffc1d50bcfd9ca1ebc22c007a23b
Carlos Sánchez <oceanrdn@gmail.com>
2023-01-09 19:24:40 +0100

    Fix add builder dark mode

--- packages/react-app/pages/admin/add-builder.jsx
+++ packages/react-app/pages/admin/add-builder.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Box, Heading, Container } from "@chakra-ui/react";
+import { Box, Heading, Container, useColorModeValue } from "@chakra-ui/react";
 import { BuilderCrudForm } from "../../components/BuilderCrudForm";
 
 export default function AddBuilderView({ mainnetProvider }) {
+  const background = useColorModeValue("#FCFBF8", "gray.800");
+
   return (
     <Container maxW="container.sm" centerContent>
       <Heading as="h1">Add Builder</Heading>
-      <Box mt={4} bgColor="#f8f8f8" py={25} px={50}>
+      <Box mt={4} bgColor={background} py={25} px={50}>
         <BuilderCrudForm mainnetProvider={mainnetProvider} />
       </Box>
     </Container>
