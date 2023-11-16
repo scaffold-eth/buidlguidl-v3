fd7c363f943396a3f53718230af3ae358eca16c6
Carlos Sánchez <oceanrdn@gmail.com>
2023-11-16 13:17:53 +0100

    Onboarding batches banner (#85)

--- packages/react-app/components/notifications/OnboardingBatch.jsx
+++ packages/react-app/components/notifications/OnboardingBatch.jsx
@@ -1,17 +1,43 @@
-import { Alert, AlertIcon, Box, CloseButton, Tooltip } from "@chakra-ui/react";
+import { Box, CloseButton, Flex, Text, Button, Link, Image, Spacer, Tooltip } from "@chakra-ui/react";
 import React from "react";
+
+const NEXT_BATCH_TELEGRAM = process.env.NEXT_PUBLIC_BATCH_TELEGRAM;
+const NEXT_BATCH_DATE = process.env.NEXT_PUBLIC_BATCH_DATE;
 
 const OnboardingBatch = ({ notification, onMarkAsRead }) => {
   return (
-    <Alert status="warning" position="relative">
-      <AlertIcon />
-      <Box>
-        <p>Onboarding Batch: {notification.id}</p>
-        <Tooltip label="Mark as Read" aria-label="Mark as Read">
-          <CloseButton position="absolute" right="4px" top="4px" onClick={() => onMarkAsRead(notification.id)} />
-        </Tooltip>
+    <Flex background="linear-gradient(180deg, #EAFFA9 24.48%, #EDEFFF 100%)" position="relative">
+      <Flex width={{ base: "100%", md: "60%" }} px="30px" py="30px" direction="column">
+        <Box maxW="200px">
+          <Image src="/assets/onboarding_batches_logo.png" alt="onboarding batches illustarion: futuristic train" wi />
+        </Box>
+        <Text fontSize="30px" fontWeight="bold" lineHeight="1.2">
+          Welcome to BuidlGuidl!
+        </Text>
+        <Text fontSize="sm" my="2">
+          To help you get started in BuidlGuidl, we've created the BG onboarding batch program. Dive into end-to-end
+          dApp development, receive mentorship from BG members, and learn how to collaborate with fellow developers in
+          open-source projects.
+        </Text>
+        <Spacer />
+        <Flex alignItems="center">
+          <Link href={NEXT_BATCH_TELEGRAM} isExternal>
+            <Button colorScheme="blue" mr="2" size="sm">
+              Join Telegram
+            </Button>
+          </Link>
+          <Text fontSize="xs">
+            Next batch starting on <strong>{NEXT_BATCH_DATE}</strong>
+          </Text>
+        </Flex>
+      </Flex>
+      <Box width="50%" display={{ base: "none", md: "inline-block" }}>
+        <Image src="/assets/onboarding_batches.png" alt="onboarding batches illustarion: futuristic train" />
       </Box>
-    </Alert>
+      <Tooltip label="Mark as Read" aria-label="Mark as Read">
+        <CloseButton position="absolute" right="4px" top="4px" onClick={() => onMarkAsRead(notification.id)} />
+      </Tooltip>
+    </Flex>
   );
 };
 
