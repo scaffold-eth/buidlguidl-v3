f03d9684a15fb51c5790f8986204013bd16cf7cf
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2024-05-08 22:02:06 +0530

    configure theme colors (#92)

--- packages/react-app/pages/index.jsx
+++ packages/react-app/pages/index.jsx
@@ -23,6 +23,7 @@ import { SERVER_URL } from "../constants";
 import { USER_FUNCTIONS } from "../helpers/constants";
 import MetaSeo from "../components/MetaSeo";
 import { getStats } from "../data/api/builder";
+import useCustomColorModes from "../hooks/useCustomColorModes";
 const buildersToShow = ["fullstack", "frontend", "damageDealer", "advisor", "artist", "support"];
 
 const StatBox = ({ value, monthlyValue, title, link }) => (
@@ -63,6 +64,8 @@ export default function Index({ bgStats }) {
   const isDarkMode = colorMode === "dark";
   const scaffoldEthBg = useColorModeValue("#fbf7f6", "whiteAlpha.300");
 
+  const { textColor } = useCustomColorModes();
+
   useEffect(() => {
     async function fetchBuilders() {
       setIsLoadingBuilders(true);
@@ -86,7 +89,7 @@ export default function Index({ bgStats }) {
         description="A curated group of Ethereum builders creating products, prototypes, and tutorials to enrich the web3 ecosytem."
         image="/assets/bg_teaser.png"
       />
-      <Flex alignItems="center" direction={{ base: "column-reverse", lg: "row" }}>
+      <Flex alignItems="center" color={textColor} direction={{ base: "column-reverse", lg: "row" }}>
         <Box w={{ base: "100%", lg: "50%" }} pl={{ base: "50px", lg: "15%" }} pr="50px" py="50px">
           <Box maxW="500px" margin={{ base: "auto", lg: "0" }} textAlign={{ base: "center", lg: "left" }}>
             <Heading as="h1" mb="10px">
--- packages/react-app/theme.js
+++ packages/react-app/theme.js
@@ -9,6 +9,28 @@ const theme = extendTheme({
     heading: "PPNeueMachina, roboto, sans-serif",
     body: "Space mono, monospace",
   },
+  colors: {
+    light: {
+      text: "#182232",
+      base: "#FFFCFC",
+      blue: "#9FA9FF",
+      baseBlue: "#D8DCFF",
+      baseBlue2: "#EDEFFF",
+      baseGreen: "#EAFFA9",
+      accentGreen: "#B5DC3A",
+      baseOrange: "#FFD4B5",
+    },
+    dark: {
+      text: "#FFFCFC",
+      base: "#0B2041",
+      blue: "#9FA9FF",
+      baseBlue: "#4B55B0",
+      baseBlue2: "#2F3679",
+      baseOrange: "#FF6E1D",
+      accentGreen: "#B5DC3A",
+      baseGreen: "#EAFFA9",
+    },
+  },
 });
 
 export default theme;
