8db47f0c3ba0df99cbb91a91731ce1438ea1be80
Shiv Bhonde <shivbhonde04@gmail.com>
2024-05-08 19:51:15 +0530

    fix: inline config

--- packages/react-app/theme.js
+++ packages/react-app/theme.js
@@ -1,8 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-  initialColorMode: "light",
-  useSystemColorMode: false,
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  },
   fonts: {
     heading: "PPNeueMachina, roboto, sans-serif",
     body: "Space mono, monospace",
