5b64973de0d3b6b5a1222b3f7f359f1485ad9a7d
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2024-05-10 19:38:16 +0530

    Hero section (#95)

---
+++ packages/react-app/components/home/HeroSection.jsx
@@ -0,0 +1,37 @@
+import { Box, Heading, Text, Image, chakra, Flex, Stack, Spacer, useColorMode } from "@chakra-ui/react";
+import StatsBox from "./StatsBox";
+
+const HeroSection = props => {
+  const { colorMode } = useColorMode();
+  const isDarkMode = colorMode === "dark";
+  return (
+    <Box mt={{ base: 20, lg: 14 }}>
+      <Flex px={12} justifyContent="center" w="full">
+        <Flex
+          maxW={{ base: "6xl", "2xl": "8xl" }}
+          alignItems="center"
+          w="full"
+          direction={{ base: "column", lg: "row" }}
+        >
+          <Stack h="full" py={4} mb={{ base: 8, lg: 0 }}>
+            <Heading as="h1" size="3xl" textAlign={{ base: "center", lg: "left" }}>
+              BuidlGuidl
+              <chakra.span fontWeight="500" fontSize="xl">
+                {" "}
+                APP v3.5
+              </chakra.span>
+            </Heading>
+            <Text maxW="md" fontSize="lg" align={{ base: "center", lg: "left" }}>
+              The BuidlGuidl ‘backoffice’, where you’ll be able to dig more into the day to day activity of the guild.
+            </Text>
+          </Stack>
+          <Spacer />
+          <StatsBox {...props} />
+        </Flex>
+      </Flex>
+      <Image src={`/assets/hero_image_${isDarkMode ? "dark" : "light"}.png`} alt="Hero Image" w="full" />
+    </Box>
+  );
+};
+
+export default HeroSection;
--- packages/react-app/pages/index.jsx
+++ packages/react-app/pages/index.jsx
@@ -1,57 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
-import NextLink from "next/link";
-import {
-  Box,
-  HStack,
-  Heading,
-  Text,
-  Link,
-  Image,
-  chakra,
-  Container,
-  Button,
-  Spinner,
-  Flex,
-  useColorModeValue,
-  useColorMode,
-  LinkOverlay,
-  LinkBox,
-} from "@chakra-ui/react";
-import BuilderFunctionList from "../components/BuilderFunctionList";
+import { Box, Link, Container, useColorModeValue, useColorMode } from "@chakra-ui/react";
 import { SERVER_URL } from "../constants";
-import { USER_FUNCTIONS } from "../helpers/constants";
 import MetaSeo from "../components/MetaSeo";
 import { getStats } from "../data/api/builder";
-import useCustomColorModes from "../hooks/useCustomColorModes";
+import HeroSection from "../components/home/HeroSection";
 const buildersToShow = ["fullstack", "frontend", "damageDealer", "advisor", "artist", "support"];
-
-const StatBox = ({ value, monthlyValue, title, link }) => (
-  <Flex
-    border="1px solid"
-    borderColor="gray.300"
-    p="20px"
-    direction="column"
-    justify="center"
-    align="center"
-    minW="140px"
-    minH="120px"
-  >
-    <Text fontSize="2xl" fontWeight="bold" whiteSpace="nowrap">
-      {link ? (
-        <NextLink href={link} passHref>
-          <LinkOverlay>{value}</LinkOverlay>
-        </NextLink>
-      ) : (
-        <>{value}</>
-      )}
-    </Text>
-    <Text color="gray.400">{title}</Text>
-    <Text fontSize="xs" color="green.500">
-      ▲ {monthlyValue}
-    </Text>
-  </Flex>
-);
 
 /* eslint-disable jsx-a11y/accessible-emoji */
 export default function Index({ bgStats }) {
@@ -87,55 +41,10 @@ export default function Index({ bgStats }) {
         description="A curated group of Ethereum builders creating products, prototypes, and tutorials to enrich the web3 ecosytem."
         image="/assets/bg_teaser.png"
       />
-      <Flex alignItems="center" direction={{ base: "column-reverse", lg: "row" }}>
-        <Box w={{ base: "100%", lg: "50%" }} pl={{ base: "50px", lg: "15%" }} pr="50px" py="50px">
-          <Box maxW="500px" margin={{ base: "auto", lg: "0" }} textAlign={{ base: "center", lg: "left" }}>
-            <Heading as="h1" mb="10px">
-              BuidlGuidl{" "}
-              <chakra.span fontWeight="500" fontSize={16}>
-                APP v3.5
-              </chakra.span>
-            </Heading>
-            <Text mb="25px" maxW="470px">
-              The BuidlGuidl ‘backoffice’, where you’ll be able to dig more into the day to day activity of the guild.
-            </Text>
-            {/*Builds / Builders / ETH distributed Ξ*/}
-            <Box d="inline-block">
-              <HStack mt="50px" justifyContent={{ base: "center", lg: "initial" }}>
-                <LinkBox>
-                  <StatBox
-                    value={bgStats.builderCount}
-                    monthlyValue={bgStats.buildersIncrementMonth}
-                    title="builders"
-                    link="/builders"
-                  />
-                </LinkBox>
-                <LinkBox>
-                  <StatBox
-                    value={bgStats.buildCount}
-                    monthlyValue={bgStats.buildsIncrementMonth}
-                    title="builds"
-                    link="/builds"
-                  />
-                </LinkBox>
-                <LinkBox onClick={() => smoothScroll(streamSection)} cursor="pointer">
-                  <StatBox
-                    value={`Ξ ${bgStats.streamedEth.toFixed(2)}`}
-                    monthlyValue={`Ξ ${bgStats.streamedEthIncrementMonth.toFixed(2)}`}
-                    title="streamed"
-                  />
-                </LinkBox>
-              </HStack>
-              <HStack mt="5px" justifyContent={{ base: "center", lg: "center" }}>
-                <Text fontSize="xs" color="green.500">
-                  ▲ Monthly change
-                </Text>
-              </HStack>
-            </Box>
-          </Box>
-        </Box>
-      </Flex>
+      {/* Hero*/}
+      <HeroSection {...bgStats} />
 
+      {/* Footer */}
       <Container maxW="container.md" centerContent>
         <Box mt="128px" mb="25px">
           🏰<b>BuidlGuidl</b> is a registered 🤠{" "}
