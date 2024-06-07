36779495f5fc51e99091840b26cc2d91a35f3eb2
Carlos Sánchez <oceanrdn@gmail.com>
2024-06-07 10:44:00 +0200

    Remove stream from builder table

--- packages/react-app/components/StreamTableCell.jsx
+++ packages/react-app/components/StreamTableCell.jsx
@@ -32,42 +32,13 @@ const BatchDisplay = ({ batch }) => {
 
 const secondsPerDay = 24 * 60 * 60;
 const BuilderStreamCell = ({ builder }) => {
-  const stream = builder?.stream;
-
-  if (!stream || !stream?.cap) {
-    return builder.builderCohort?.length || builder.builderBatch ? (
-      <>
-        <BatchDisplay batch={builder.builderBatch} />
-        <CohortDisplay cohorts={builder.builderCohort} />
-      </>
-    ) : (
-      <Center>-</Center>
-    );
-  }
-
-  const cap = ethers.utils.parseUnits(stream.cap);
-  const frequency = stream.frequency;
-  const last = stream.lastContract;
-  const frequencyDays = frequency / secondsPerDay;
-  const unlockedPercentage = (new Date().getTime() / 1000 - last) / frequency;
-  const unlockedAmount = cap.mul(Math.round(new Date().getTime() / 1000 - last)).div(frequency);
-  const available = cap.lt(unlockedAmount) ? cap : unlockedAmount;
-
-  const capStr = ethers.utils.formatEther(cap);
-  const availableStr = ethers.utils.formatEther(available);
-  return (
-    <Flex align="center" justify="center" direction="column">
-      <Flex align="center" justify="center" direction="column" px={4} mt={4}>
-        <Box mb={1} whiteSpace="nowrap">
-          Ξ {parseFloat(availableStr).toFixed(4)} / {parseFloat(capStr).toFixed(1)} @ {frequencyDays}d
-        </Box>
-        <Box w="full" pl={1}>
-          <Progress flexShrink={1} size="xs" value={unlockedPercentage * 100} colorScheme="green" />
-        </Box>
-      </Flex>
+  return builder.builderCohort?.length || builder.builderBatch ? (
+    <>
       <BatchDisplay batch={builder.builderBatch} />
       <CohortDisplay cohorts={builder.builderCohort} />
-    </Flex>
+    </>
+  ) : (
+    <Center>-</Center>
   );
 };
 
