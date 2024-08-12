21355cfe8c306f9691b061621af423238a0f65e4
ByteAtATime <ByteAtATime@proton.me>
2024-08-12 10:03:22 -0700

    feat: use build type constants to create colored BuildTypeBadge.jsx

--- packages/react-app/components/BuildTypeBadge.jsx
+++ packages/react-app/components/BuildTypeBadge.jsx
@@ -1,12 +1,21 @@
 import { Badge } from "@chakra-ui/react";
+import { BUILD_TYPES } from "../helpers/constants";
 
 export default function BuildTypeBadge({ type, ...badgeProps }) {
-    switch (type) {
-        case "dapp":
-            return <Badge colorScheme="orange" {...badgeProps}>DApp</Badge>;
-        case "extension":
-            return <Badge colorScheme="blue" {...badgeProps}>Extension</Badge>;
-        default:
-            throw new Error(`Unknown build type: ${type}`);
+    const colorSchemes = {
+        dapp: "orange",
+        extension: "blue",
+        challenge: "green",
+        design: "purple",
+        devrel: "pink",
+        other: "gray"
+    };
+
+    for (const [key, label] of Object.entries(BUILD_TYPES)) {
+        if (type === key) {
+            return <Badge colorScheme={colorSchemes[key]} {...badgeProps}>{label}</Badge>;
+        }
     }
+
+    throw new Error(`Unknown build type: ${type}`);
 }
