fe009f1966f565916fd12edad720fdf89eec2bec
phipsae <philip.krause88@yahoo.de>
2024-10-17 10:19:48 -0400

    Batch builders page fix count order name stream (#115)

---
+++ packages/react-app/components/batch-builders/BatchBuilderNumberCell.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Badge, Center, Link } from "@chakra-ui/react";
+import { BATCH_STATUS } from "../../helpers/constants";
+
+const BuilderBatchNumberCell = ({ batch }) => {
+  if (!batch || batch?.number === "") return null;
+
+  return (
+    <Center mt={2}>
+      <Badge colorScheme={batch.status === BATCH_STATUS.GRADUATE ? "green" : "orange"} textAlign="center">
+        Batch #{batch.number}
+      </Badge>
+    </Center>
+  );
+};
+
+export default BuilderBatchNumberCell;
