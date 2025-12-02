b34fadc4f86cbd803c8bbc1c4339a75f3748033e
Carlos Sánchez <oceanrdn@gmail.com>
2025-12-02 11:16:59 +0100

    Use new BG RPC

--- packages/react-app/pages/_app.js
+++ packages/react-app/pages/_app.js
@@ -94,7 +94,7 @@ function MyApp({ Component, pageProps }) {
 
     if (DEBUG) console.log("📡 Connecting to Mainnet Ethereum");
     const scaffoldEthProviderPromise = providerPromiseWrapper(
-      new StaticJsonRpcProvider("https://rpc.scaffoldeth.io:48544"),
+      new StaticJsonRpcProvider("https://mainnet.rpc.buidlguidl.com"),
     );
 
     // attempt to connect to our own scaffold eth rpc and if that fails fall back to infura...
